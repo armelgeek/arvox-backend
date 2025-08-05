38e5fa1be286b8c3b42f0f9b6151958dd4127dd3
Hajavololona Armellin <armelgeek5@gmail.com>
2025-08-05 21:09:57 +0300

    Add TypeScript type definitions for arvox-backend
    Introduces a new types package with index.d.ts exporting core, interfaces, types, and utils from the arvox-backend framework. Includes a package.json for publishing type definitions.

---
+++ types/index.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for arvox-backend
+// Project: https://github.com/armelgeek/arvox-backend
+// Definitions by: Arvox
+
+export * from '../dist/core';
+export * from '../dist/interfaces';
+export * from '../dist/types';
+export * from '../dist/utils';
+export { ArvoxFramework } from '../dist/core/arvox-framework';
+export declare const FRAMEWORK_VERSION: string;
+export declare const FRAMEWORK_NAME: string;
